3a4bc4800833fe0515d742e45a496b2bc80355e5
yanminghao <2540898831@qq.com>
2020-12-17 16:29:17 +0800

    格式化代码

--- src/main/java/com/sirius/bean/Employee.java
+++ src/main/java/com/sirius/bean/Employee.java
@@ -8,14 +8,29 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class Employee {
-    //主键
+
+    /**
+     * 主键
+     */
     private Integer id;
-    //姓名
+
+    /**
+     * 姓名
+     */
     private String lastName;
-    //邮箱
+
+    /**
+     * 邮箱
+     */
     private String email;
-   //性别
+
+    /**
+     * 性别
+     */
     private Integer gender;
-    //年龄
+
+    /**
+     * 年龄
+     */
     private Integer age;
 }
